88271ed50c8d6f111a80afdd5ea3c451397ee726
Victor Emanuel <victorgit28@outlook.com>
2024-10-03 10:16:27 -0300

    Verificando reservatorio

--- reservatorio/Program.cs
+++ reservatorio/Program.cs
@@ -4,3 +4,9 @@ bool boia1 = false;
 bool boai2 = false;
 bool boaiCaixa = false;
 bool valvula = false;
+
+string exibir;
+exibir = boai2 == false ? "Enchendo o reservatorio de água!" : "O reservatorio de água está cheio!";
+Console.WriteLine(exibir);
+
+
