870f8dff291d35da7fec2d1ce45236aca7e26f3a
Victor Emanuel <victorgit28@outlook.com>
2024-10-03 10:19:43 -0300

    Verificando acionamento da bomba

--- reservatorio/Program.cs
+++ reservatorio/Program.cs
@@ -5,8 +5,13 @@ bool boai2 = false;
 bool boaiCaixa = false;
 bool valvula = false;
 
-string exibir;
+string exibir, exibir2;
 exibir = boai2 == false ? "Enchendo o reservatorio de água!" : "O reservatorio de água está cheio!";
 Console.WriteLine(exibir);
+
+exibir2 = boia1 && boaiCaixa == false ? "Enchendo a caixa de água" : "A caixa de água está cheia";
+Console.WriteLine(exibir2);
+
+
 
 
